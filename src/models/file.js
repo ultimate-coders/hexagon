2a92e3d2cdd49201ed6547ecc14bb208b9bdc527
AnwarAbbass <9982017726.UPS@htu.edu.jo>
2021-06-12 08:59:26 +0300

    delete v2

--- src/models/file.js
+++ src/models/file.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const client = require('./db');
+const Client = require('./db');
 
 async function saveFile(fileUrl) {
   try {
     let sqlQuery = 'INSERT INTO file (file) VALUES ($1) RETURNING *;';
     let safeValues = [fileUrl];
-    let fileData = await client.query(sqlQuery, safeValues);
+    let fileData = await Client.query(sqlQuery, safeValues);
     return fileData[0];
   } catch (e) {
     throw new Error(e);
--- src/routes/v1.js
+++ src/routes/v1.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {createMessageHandler, getMessageHandler, deleteMessageHandler,updateMessageHandler} = require('../controllers/messageControllers');
 const {createNotificationHandler, getNotificationHandler, updateNotificationHandler} = require('../controllers/notificationController');
+const {getPostCommentsHandler,createCommentHandler,updateCommentHandler,deleteCommentHandler}=require('../controllers/commentController');
+const followHndler=require('../controllers/followController');
 
 router.post('/messages', createMessageHandler);
 router.get('/messages', getMessageHandler);
@@ -12,7 +14,12 @@ router.post('/notifications', createNotificationHandler);
 router.get('/notifications', getNotificationHandler);
 router.put('/notifications/:id', updateNotificationHandler);
 
+router.get('/comment/:postId',getPostCommentsHandler);
+router.post('/comment',createCommentHandler);
+router.put('/comment',updateCommentHandler);
+router.delete('/comment/:id',deleteCommentHandler);
 
+router.post('/follow',followHndler);
 
 router.get('/test', (req,res)=>{
   res.send('working well');
