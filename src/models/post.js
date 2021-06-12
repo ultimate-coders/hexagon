75c4e9ee06f64687674f52f16d800fe40985b8bc
Wesam Al-Masri <mr0virus@gmail.com>
2021-06-12 13:17:48 +0300

    post routes and models

--- src/models/post.js
+++ src/models/post.js
@@ -7,13 +7,19 @@ const { PAGE_SIZE } = require('../configurations');
 // For creating a profile record
 function UserPost(postObj) {
   this.category_id = postObj.category_id;
-  this.user_id = postObj.user_id;
+  this.profile_id = postObj.profile_id;
   this.text = postObj.text;
-  this.attachment_id = postObj.attachment_id;
+}
+
+// For formatting the image file response
+function Image(imageObj) {
+  this.id = imageObj.file_id;
+  this.link = imageObj.link;
 }
 
 // To format the response
 function Post(post) {
+  this.id = post.post_id;
   this.text = post.text;
   this.category = {
     id: post.category_id,
@@ -34,42 +40,46 @@ function Post(post) {
       email: post.email,
     },
   };
-  this.images = [];
+  this.images = [{
+    id: post.image_id,
+    link: post.image_link,
+  }];
 }
 
 // Get all posts
 async function getAllPosts(categoryName = '', pageNumber = 1) {
   try {
     let sqlQuery = `
-    SELECT post.id AS post_id, profile.id AS profile_id, user_file.id AS file_id, file AS profile_picture, client.id AS user_id, category.id AS category_id, name AS category_name, text, first_name, last_name, caption, file as profile_picture, user_name, email FROM post JOIN profile ON post.profile_id = profile.id JOIN client ON client.id = profile.user_id JOIN category ON post.category_id = category.id JOIN user_file ON profile.profile_picture = user_file.id ORDER BY post.id DESC LIMIT $1 OFFSET $2;
+    SELECT post.id AS post_id, profile.id AS profile_id, profile_image.id AS file_id, profile_image.file AS profile_picture, client.id AS user_id, category.id AS category_id, name AS category_name, text, first_name, last_name, caption, user_name, email, post_image.id AS image_id, post_image.file AS image_link FROM post JOIN profile ON post.profile_id = profile.id JOIN client ON client.id = profile.user_id JOIN category ON post.category_id = category.id JOIN user_file AS profile_image ON profile.profile_picture = profile_image.id JOIN attachment ON attachment.post_id = post.id JOIN user_file AS post_image ON attachment.file_id = post_image.id ORDER BY post.id DESC LIMIT $1 OFFSET $2;
     `;
     let startFrom = (pageNumber - 1) * PAGE_SIZE;
-    let safeValues = [PAGE_SIZE, startFrom];
+    let safeValues = [PAGE_SIZE + 1, startFrom];
     // Filtering
     if(categoryName && categoryName !== ''){
       sqlQuery = `
-      SELECT post.id AS post_id, profile.id AS profile_id, user_file.id AS file_id, file AS profile_picture, client.id AS user_id, category.id AS category_id, name AS category_name, text, first_name, last_name, caption, file as profile_picture, user_name, email FROM post JOIN profile ON post.profile_id = profile.id JOIN client ON client.id = profile.user_id JOIN category ON post.category_id = category.id JOIN user_file ON profile.profile_picture = user_file.id WHERE category_name = $1 ORDER BY post.id DESC LIMIT $2 OFFSET $3;
+      SELECT post.id AS post_id, profile.id AS profile_id, profile_image.id AS file_id, profile_image.file AS profile_picture, client.id AS user_id, category.id AS category_id, name AS category_name, text, first_name, last_name, caption, user_name, email, post_image.id AS image_id, post_image.file AS image_link FROM post JOIN profile ON post.profile_id = profile.id JOIN client ON client.id = profile.user_id JOIN category ON post.category_id = category.id JOIN user_file AS profile_image ON profile.profile_picture = profile_image.id JOIN attachment ON attachment.post_id = post.id JOIN user_file AS post_image ON attachment.file_id = post_image.id WHERE category.name = $1 ORDER BY post.id DESC LIMIT $2 OFFSET $3;
       `;
       startFrom = (pageNumber - 1) * PAGE_SIZE;
-      safeValues = [categoryName, PAGE_SIZE, startFrom];
+      safeValues = [categoryName, PAGE_SIZE + 1, startFrom];
     }
     // Query the database
     const postsData = await client.query(sqlQuery, safeValues);
-    console.log(postsData.rows);
-    const hasNext = parseInt(postsData.rowCount) > startFrom + PAGE_SIZE;
+    const hasNext = postsData.rowCount > PAGE_SIZE;
+    let results = postsData.rows.map(post => new Post(post));
+    if(hasNext)  results = results.slice(0, -1);
     const response = {
-      results: postsData.rows.map(post => new Post(post)),
-      count: postsData.rowCount,
-      hasNext,
+      count: results.length,
+      hasNext: hasNext,
+      results: results,
     };
-    return response;   
+    return response; 
   } catch (e) {
     throw new Error(e);
   }
 }
 
 // Get single post
-async function getSinglePost(id, pageNumber = 1) {
+async function getSinglePost(id) {
   try {
     let sqlQuery = `
     SELECT post.id AS post_id, profile.id AS profile_id, user_file.id AS file_id, file AS profile_picture, client.id AS user_id, category.id AS category_id, name AS category_name, text, first_name, last_name, caption, file as profile_picture, user_name, email FROM post JOIN profile ON post.profile_id = profile.id JOIN client ON client.id = profile.user_id JOIN category ON post.category_id = category.id JOIN user_file ON profile.profile_picture = user_file.id WHERE post.id = $1;
@@ -78,7 +88,12 @@ async function getSinglePost(id, pageNumber = 1) {
     let safeValues = [id];
     // Query the database
     const postsData = await client.query(sqlQuery, safeValues);
-    const post = new UserPost(postsData.rows[0]);
+    const post = new Post(postsData.rows[0]);
+    let imageSqlQuery = `
+    SELECT user_file.id AS file_id, file AS link FROM attachment JOIN user_file ON attachment.file_id = user_file.id WHERE attachment.post_id = $1;
+    `;
+    let attachments = await client.query(imageSqlQuery, safeValues);
+    post['images'] = attachments.rows.map(image => new Image(image));
     return post;   
   } catch (e) {
     throw new Error(e);
@@ -89,29 +104,77 @@ async function getSinglePost(id, pageNumber = 1) {
 async function createPost(postObj) {
   try {
     let sqlQuery = `
-    INSERT INTO post (profile_id, text, category_id) VALUES ($1, $2, $3);
+    INSERT INTO post (profile_id, text, category_id) VALUES ($1, $2, $3) RETURNING id;
     `;
     let post = new UserPost(postObj);
     let safeValues = [post.profile_id, post.text, post.category_id];
     // Query the database
     const postsData = await client.query(sqlQuery, safeValues);
-    return post;   
+    let attachmentsSqlQuery = 'INSERT INTO attachment (post_id, file_id) VALUES ';
+    safeValues = [postsData.rows[0].id];
+    if(postObj.images && postObj.images.length > 0){
+      postObj.images.forEach((image_id, i) => {
+        if(i > 0) attachmentsSqlQuery += ',';
+        attachmentsSqlQuery += `($1, $${i+2}) `;
+        safeValues.push(image_id);
+      });
+      attachmentsSqlQuery += ';';
+    }
+
+    let attachmentData = await client.query(attachmentsSqlQuery, safeValues);
+    
+    return attachmentData.rows;   
   } catch (e) {
     throw new Error(e);
   }
 }
 
-// Create post
+// Update post
 async function updatePost(id, postObj) {
   try {
     let sqlQuery = `
-    UPDATE post SET text = $1, category_id = $2 WHERE id = $3 RETURNING *;
+    UPDATE post SET text = $1, category_id = $2 RETURNING id;
     `;
     let post = new UserPost(postObj);
     let safeValues = [post.text, post.category_id];
     // Query the database
     const postsData = await client.query(sqlQuery, safeValues);
-    return post;   
+    let attachmentsSqlQuery = 'INSERT INTO attachment (post_id, file_id) VALUES ';
+    safeValues = [postsData.rows[0].id];
+    
+    if(postObj.images && postObj.images.length > 0){
+      let deleteAttachmentQuery = `
+      DELETE FROM attachment WHERE id = $1;
+      `;
+      await client.query(deleteAttachmentQuery, [postsData.rows[0].id]);
+
+      postObj.images.forEach((image_id, i) => {
+        if(i > 0) attachmentsSqlQuery += ',';
+        attachmentsSqlQuery += `($1, $${i+2}) `;
+        safeValues.push(image_id);
+      });
+      attachmentsSqlQuery += ';';
+    }
+
+    let attachmentData = await client.query(attachmentsSqlQuery, safeValues);
+    
+    return attachmentData.rows;   
+  } catch (e) {
+    throw new Error(e);
+  }
+}
+
+
+// Update post
+async function deletePost(id) {
+  try {
+    let sqlQuery = `
+    DELETE FROM post WHERE id = $1;
+    `;
+    let safeValues = [id];
+
+    let response = await client.query(sqlQuery, safeValues);
+    return response.rows;
   } catch (e) {
     throw new Error(e);
   }
@@ -123,4 +186,5 @@ module.exports = {
   getSinglePost,
   createPost,
   updatePost,
+  deletePost,
 };
